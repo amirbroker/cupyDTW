4a274bccb8bb090106ec4556c9abfd16550b6f5e
amirbroker <42622263+amirbroker@users.noreply.github.com>
2025-02-22 00:59:36 +0330

    Update DTWs.py

--- DTWs.py
+++ DTWs.py
@@ -1,4 +1,44 @@
-num_entries = 2345000
-lenSeq =600
-lenTrg =825
+from config.crtDTWvrbs import *
+from config.paths import *
+import os
 
+seqNameList = seqNameList(sequencesPath)
+lenOfEachSeqList = lenOfEachSeqList(seqNameList)
+numOfEachSeqInFileList = numOfEachSeqInFileList(
+    seqNameList, lenOfEachSeqList, sequencesPath
+)
+trgNameList = trgNameList(targetpath)
+######################################################################## parameter for create cuda files
+lenTrg = [int(x.replace(".bin", "")) for x in trgNameList][0]
+
+errLen = []
+for seqIND in range(2):
+    num_entries = numOfEachSeqInFileList[seqIND]
+    lenSeq = lenOfEachSeqList[seqIND]
+    createCudaFileBaseOfSeqANDTrg(
+        num_entries,
+        lenTrg,
+        lenSeq,
+        dtwcudaname,
+        configpath,
+        dtwcudapath,
+        maincudaname,
+        sequencesPath,
+        targetpath,
+        resultspath,
+        maincudapath,
+    )
+    runMain = os.system(
+        "nvcc -O3 -std=c++14 -arch=sm_89 -Xcompiler='-fopenmp -march=native' /home/amir/cupyDTW/cuDTW/main.cu -o main"
+    )
+    cmpMain = os.system("./main")
+    if runMain == cmpMain == 0:
+        # os.remove("main")
+        pass
+    else:
+        errLen.append(lenSeq)
+
+
+with open("errFile.txt", "w") as f:
+    for line in errLen:
+        f.write(f"{line}\n")
